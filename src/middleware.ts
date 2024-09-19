74d8720f1b32c80e1d5670d9b16d115ed77d8e2b
Design Hub <atendimento.designhub@gmail.com>
2024-09-19 17:38:08 +0000

    feat: Supabase integration, ShadCNUI, and various configurations
    Integrated Supabase for backend services and ShadCNUI for UI components. Several configuration files were modified to support the new stack, including the environment setup (env), Tailwind configuration, and middleware for Supabase authentication. Additionally, added new components such as button.tsx and utility files (client.ts, utils.ts) to streamline application development with the new UI and backend integration.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,50 +1,24 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-import type { NextFetchEvent, NextRequest } from 'next/server';
-import createMiddleware from 'next-intl/middleware';
+import type { NextRequest } from "next/server";
+import createMiddleware from "next-intl/middleware";
 
-import { AppConfig } from './utils/AppConfig';
+import { AppConfig } from "./utils/AppConfig";
+import { updateSession } from "./lib/supabase/middleware";
 
 const intlMiddleware = createMiddleware({
-  locales: AppConfig.locales,
-  localePrefix: AppConfig.localePrefix,
-  defaultLocale: AppConfig.defaultLocale,
+ locales: AppConfig.locales,
+ localePrefix: AppConfig.localePrefix,
+ defaultLocale: AppConfig.defaultLocale,
 });
 
-const isProtectedRoute = createRouteMatcher([
-  '/dashboard(.*)',
-  '/:locale/dashboard(.*)',
-]);
-
-export default function middleware(
-  request: NextRequest,
-  event: NextFetchEvent,
+export default async function middleware(
+ request: NextRequest,
+ //event: NextFetchEvent,
 ) {
-  // Run Clerk middleware only when it's necessary
-  if (
-    request.nextUrl.pathname.includes('/sign-in')
-    || request.nextUrl.pathname.includes('/sign-up')
-    || isProtectedRoute(request)
-  ) {
-    return clerkMiddleware((auth, req) => {
-      if (isProtectedRoute(req)) {
-        const locale
-          = req.nextUrl.pathname.match(/(\/.*)\/dashboard/)?.at(1) ?? '';
+ await updateSession(request);
 
-        const signInUrl = new URL(`${locale}/sign-in`, req.url);
-
-        auth().protect({
-          // `unauthenticatedUrl` is needed to avoid error: "Unable to find `next-intl` locale because the middleware didn't run on this request"
-          unauthenticatedUrl: signInUrl.toString(),
-        });
-      }
-
-      return intlMiddleware(req);
-    })(request, event);
-  }
-
-  return intlMiddleware(request);
+ return intlMiddleware(request);
 }
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next|monitoring).*)', '/', '/(api|trpc)(.*)'], // Also exclude tunnelRoute used in Sentry from the matcher
+ matcher: ["/((?!.+\\.[\\w]+$|_next|monitoring).*)", "/", "/(api|trpc)(.*)"], // Also exclude tunnelRoute used in Sentry from the matcher
 };
