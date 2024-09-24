e6e85e7fe93ed2c49308e16df379cb8f2ccbd020
Design Hub <atendimento.designhub@gmail.com>
2024-09-24 17:43:03 +0000

    rename files

---
+++ src/app/[locale]/(unauth)/designs/[categoria]/page.tsx
@@ -0,0 +1,7 @@
+import Link from "next/link";
+
+export default function Category({ params }: { params: { category: string } }) {
+  return <div>My Category: {params.category}
+  <Link href="/designs/hamburgueria/pack-design-hamburgueria"/>
+  </div>
+}
--- src/app/[locale]/(unauth)/designs/page.tsx
+++ src/app/[locale]/(unauth)/designs/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Categories(){
  return(
 <main>
   <h1>Coleções</h1>
+  <Link href="/designs/hamburgueria/"/>
 </main>
  )
 }
